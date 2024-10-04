d94c450e3605a5acf4f3430c729ebb75cf2d70d2
Surya Sharma <surya.sharma233@gmail.com>
2024-10-04 15:07:35 -0500

    added wrapped err type

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,19 @@ use reqwest::Method;
 use std::collections::HashMap;
 use serde::{Deserialize, Serialize};
 use std::env;
+use std::fmt::Display;
+
+pub const API_KEY_ENV_VAR: &str = "MASKED_EMAIL_TOKEN";
+pub const ACC_ID_KEY_ENV_VAR: &str = "MASKED_EMAIL_ACC_ID";
+
+pub const USING_CORE: &str = "urn:ietf:params:jmap:core";
+pub const USING_MASKED: &str = "https://www.fastmail.com/dev/maskedemail";
+
+pub const BASE_URL: &str = "https://api.fastmail.com/jmap/api/";
+
+pub const MASKEDEMAIL_GET_METHOD_TYPE: &str = "MaskedEmail/get";
+pub const ACC_ID_PARAM_KEY: &str = "accountId";
+
 
 #[derive(Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
@@ -11,29 +24,73 @@ struct JMAPReqBody {
     pub method_calls: Vec<(String, HashMap<String, String>, String)>
 }
 
-#[tokio::main]
-async fn main() {
-    get_masked_email().await;
+#[derive(Debug)]
+enum JMAPError {
+    VarError(env::VarError),
+    Error(reqwest::Error)
 }
 
-async fn get_masked_email() {
-    let api_key = env::var("MASKED_EMAIL_TOKEN").unwrap();
+impl Display for JMAPError {
+    fn fmt(&self, f: & mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            JMAPError::Error(e) => {
+                write!(f, "{}", e)
+            },
+            JMAPError::VarError(e) => {
+                write!(f, "{}", e)
+            }
+        }
+    }
+}
+
+impl From<reqwest::Error> for JMAPError {
+    fn from(err: reqwest::Error) -> Self {
+        JMAPError::Error(err)
+    }
+}
+
+impl From<std::env::VarError> for JMAPError {
+    fn from(err: std::env::VarError) -> Self {
+        JMAPError::VarError(err)
+    }
+}
+
+impl std::error::Error for JMAPError {}
+
+#[tokio::main]
+async fn main() {
+    let result = get_masked_email().await;
+    let _ = match result {
+        Err(e) => { dbg!(e) },
+        Ok(_) => todo!()
+    };
+}
+
+async fn get_masked_email() -> Result<(), JMAPError> {
+    let api_key = env::var(API_KEY_ENV_VAR)?;
     let client = reqwest::Client::new();
-    let acc_id = env::var("MASKED_EMAIL_ACC_ID").unwrap();
+    let acc_id = env::var(ACC_ID_KEY_ENV_VAR)?;
 
     let req_body = JMAPReqBody {
-        using: vec!["https://www.fastmail.com/dev/maskedemail".to_string(), "urn:ietf:params:jmap:core".to_string()], 
-        method_calls: vec![("MaskedEmail/get".to_string(), HashMap::from([("accountId".to_string(), acc_id)]), "0".to_string())] 
+        using: vec![USING_MASKED.to_string(), USING_MASKED.to_string()], 
+        method_calls: vec![(
+             MASKEDEMAIL_GET_METHOD_TYPE.to_string(),
+             HashMap::from([(ACC_ID_PARAM_KEY.to_string(), acc_id.to_string())]),
+             "0".to_string())
+        ]
     };
 
     let request_mask = client.request(Method::POST, "https://api.fastmail.com/jmap/api/")
         .bearer_auth(api_key)
         .header("Content-Type", "application/json")
         .body(serde_json::to_string(&req_body).unwrap())
-        .build().unwrap();
+        .build().unwrap(); //never returns an error
 
 
-    let body = client.execute(request_mask).await.unwrap().text().await.unwrap();
+    let body = client.execute(request_mask).await?;
+    //let body = client.execute(request_mask).await.unwrap().text().await.unwrap();
+    let body_txt = body.text().await.unwrap();
 
-    println!("body = {body:?}");
+    println!("body = {body_txt:?}");
+    Ok(())
 }
